a6056121a6098da96f950b97f81a22b28a117b00
Darlei_Midoria <78001770+Darleysantana@users.noreply.github.com>
2021-04-05 21:35:52 -0300

    Create Valordefaturamento.cpp

---
+++ Valordefaturamento.cpp
@@ -0,0 +1,39 @@
+#include stdio;
+
+int main(){
+
+double diario['500,00', '20,00', '50,00','100','10,00','30,00'];
+double maior;
+double menor;
+double media;
+
+printf("Insira o valor do faturamento do dia: ");
+scanf("%d" diario[]);
+maior = diario[0];
+for(cont = 0; i == cont; cont++)
+{
+    i++
+    if(maior < diario[i])
+    {
+        maior = diario[i];
+     }
+
+}
+printf("O maior faturamento %d" &maior);
+
+for(cont = 0; i == cont; cont++)
+{
+    i++
+   if(menor > diario[i])
+        menor = diario[i];
+}
+printf("O menor faturamento foi:  %d" & menor);
+
+for(cont = 0; i == cont; cont++)
+{
+      i = 0;
+   media = diario[i] + diario[i++];
+}
+media = media/i;
+printf("Esse foi a media %d" &media);
+}
